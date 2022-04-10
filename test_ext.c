f6ef3dd8eed4f1af4be45c780b03aa739bc26825
Ashish <atondwalstudio@gmail.com>
2022-04-10 13:56:28 -0700

    ext file

---
+++ test_ext.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("Hello From Ashish's New Linux Machine!\n");
+	printf("I'm outside the repo using a hard link\n");
+	return 0;
+}
