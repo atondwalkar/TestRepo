2d735ff8554e0f8fc8f528066041320a7b965094
“Ashish” <atondwalstudio@gmail.com>
2022-04-11 11:51:57 -0700

    updating tcl

--- upload.tcl
+++ upload.tcl
@@ -1,10 +1,6 @@
-proc git_add {file_ext file_type folder} {
-  try {
-    set results [exec -ignorestderr git add $file_ext]
-    puts $file_ext
-  } trap CHILDSTATUS {results options} {
-    puts "Warning: No $file_type files update has been found in $folder"
-  }
+proc git_add {file_ext} {
+	set results [exec -ignorestderr git add $file_ext]
+	puts "	$file_ext"
 }
 
 exec git remote set-url origin https://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@github.com/atondwalkar/TestRepo.git
@@ -13,8 +9,8 @@ exec git config --global user.name “Ashish”
 
 puts "Adding files to push:"
 set path pwd
-git_add "test.c" "C" "$path"
-git_add "upload.tcl" "tcl" "$path"
+git_add "*.c"
+git_add "upload.tcl"
 
 puts "Commit message: "
 exec git commit -m [gets stdin]
