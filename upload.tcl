b0e0c2b9242a52dea584f2dd87903b5894ce756b
“Ashish” <atondwalstudio@gmail.com>
2022-04-11 11:45:17 -0700

    updating tcl script

--- upload.tcl
+++ upload.tcl
@@ -7,7 +7,7 @@ proc git_add {file_ext file_type folder} {
   }
 }
 
-exec git remote set-url origin https://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@github.com/atondwalkar/TestRepo.git
+exec git remote set-url origin https://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@github.com/atondwalkar/TestRepo.git
 exec git config --global user.email atondwalstudio@gmail.com
 exec git config --global user.name “Ashish”
 
