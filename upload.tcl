8887d6442292970b0ad7e4bbf5fca6e4b91ad728
atondwalkar <atondwalstudio@gmail.com>
2022-04-18 11:47:51 -0700

    Update upload.tcl

--- upload.tcl
+++ upload.tcl
@@ -3,10 +3,6 @@ proc git_add {file_ext} {
 	puts "	$file_ext"
 }
 
-#exec git remote set-url origin https://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@github.com/atondwalkar/TestRepo.git
-exec git config --global user.email atondwalstudio@gmail.com
-exec git config --global user.name “Ashish”
-
 puts "Adding files to push:"
 set path pwd
 git_add "*.c"
